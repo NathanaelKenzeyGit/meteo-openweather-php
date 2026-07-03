e366c4da644419b5d1d32c7025dc0186c72e992a
nathanael kenzey <nathanael75kenzey@gmail.com>
2026-07-03 22:35:26 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/style/output.css">
+    <meta http-equiv="refresh" content="3">
+    <title>Projet Météo</title>
+</head>
+<body>
+    <header>
+        <nav>
+            
+        </nav>
+    </header>
+    <main>
+        <div class="container mx-auto flex flex-col gap-6">
+            <h1 class="text-3xl text-center text-blue-400">La méteo avec l'api de Open Weather</h1>
+                    <div class="border-2 border-gray-400 p-5 ">
+            <h2>Prévisions 5 jours</h2>
+            <div class="card_container flex">
+<article class="card border-2 p-3">
+    <p class="card__header">Lun</p>
+</article>           
+        </div>
+
+    </main>
+</body>
+</html>
